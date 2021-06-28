2f6f0e386f7e36a945675d7502e03f82daccb946
aKn8194 <knguyenanh8194@gmail.com>
2021-06-28 17:15:48 +0700

    Update chapter 6

---
+++ Automate boring stuff with Python/6. Manipulating strings/printTable.py
@@ -0,0 +1,12 @@
+tableData = [
+    ['apples', 'oranges', 'cherries', 'banana'],
+    ['Alice', 'Bob', 'Carol', 'David'],
+    ['dogs', 'cats', 'moose', 'goose']
+]
+
+col_widths = []
+for i, record in enumerate(tableData):
+    col_widths.insert(i, max(len(item) for item in record))
+
+for i in range(len(tableData[0])):
+    print(' '.join(record[i].rjust(col_widths[j]) for j, record in enumerate(tableData)))
--- Automate boring stuff with Python/Test/hello.py
+++ Automate boring stuff with Python/Test/hello.py
@@ -1,1 +1,3 @@
-print("Hello there!\nHow are you?\nI\'m doing fine.")
+names = ['Bob', 'Alice', 'Guido']
+for index, value in enumerate(names):
+    print(f'{index}: {value}')
