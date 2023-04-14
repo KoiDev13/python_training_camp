e821b65813c7c3fa930de5154c187b79dc318b94
khoi.nguyen <knguyenanh8194@gmail.com>
2023-04-14 11:38:53 +0700

    add convention content

---
+++ convention/main.py
@@ -0,0 +1,8 @@
+from .sub_main import say_hi, say_hello
+
+import datetime
+
+# Press the green button in the gutter to run the script.
+say_hi('huuhai_nguyen')
+
+
---
+++ convention/submain.py
@@ -0,0 +1,9 @@
+def say_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def say_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
