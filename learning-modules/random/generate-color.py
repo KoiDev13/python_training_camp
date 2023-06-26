095f7077cd84fa9f982878066b9ca682465d82de
khoi.nguyen <knguyenanh8194@gmail.com>
2023-06-26 10:02:14 +0700

    addi generate color

---
+++ learning-modules/random/generate-color.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program to generate
+a random color hex, 
+a random alphabetical string,
+random value between two integers (inclusive)
+and a random multiple of 7 between 0 and 70. 
+"""
+
+import random
+
+result = random.randint()
+print(result)
