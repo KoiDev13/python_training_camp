241d1675cfc217e4952c8c944967ba8ea07be14b
khoi.nguyen <knguyenanh8194@gmail.com>
2023-07-18 10:46:14 +0700

    adding new file

--- hackerrank/flippingMatrix.py
+++ hackerrank/flippingMatrix.py
@@ -1,11 +1,47 @@
-matrix = [
-    [112, 42, 83, 119],
-    [56, 125, 56, 49],
-    [15, 78 , 101, 43],
-    [62, 98, 114, 108]
-]
+#!/bin/python3
 
-new_matrix = []
+import math
+import os
+import random
+import re
+import sys
 
-for i in matrix:
-    print(i[::-1])
+
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(matrix):
+    # Write your code here
+    max_total = 0
+    n = int(len(matrix)/2)
+    
+    for i in range(n):
+        for j in range(n):
+            max_s = max(matrix[i][j], matrix[i][(2*n-1)-j], matrix[(2*n-1)-i][j], matrix[(2*n-1)-i][(2*n-1)-j])
+            max_total += max_s
+    
+    return max_total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        matrix = []
+
+        for _ in range(2 * n):
+            matrix.append(list(map(int, input().rstrip().split())))
+
+        result = flippingMatrix(matrix)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ programiz/datastructureAlgori/lecture/map.py
@@ -0,0 +1,3 @@
+c=[3,2,5,6]
+c=list(map(lambda x: x**2, c))
+print(c)
