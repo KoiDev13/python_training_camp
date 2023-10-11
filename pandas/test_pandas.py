ac71235747c04be95d6bed5555d0082413382a3d
khoi.nguyen <knguyenanh8194@gmail.com>
2023-10-11 16:48:02 +0700

    add pandas exercises

---
+++ pandas/test_pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    'country': ['Brazil', 'Russia', 'India', 'China', 'South Africa'],
+    'capital': ['Brasilia', 'Moscow', 'New Delhi', 'Beijing', 'Pretoria'],
+    'area': [8.516, 17.100, 3.286, 9.597, 1.221],
+    'population': [200.40, 143.50, 1252.00, 1357.00, 52.98]
+}
+
+df = pd.DataFrame(data)
+filter_country = df[df['country'] == 'Brazil']
+print(filter_country)
